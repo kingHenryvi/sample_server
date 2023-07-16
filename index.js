b7437b0cdeee1df6a2878251b03173dd53a3e430
kingHenryvi <msalcantara@gmail.com>
2023-07-16 14:43:57 +0800

    fix

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-import express from "express";
-import cors from "cors";
-import "./loadEnvironment.mjs";
-import router from "./routes/record.mjs";
+// import express from "express";
+// import cors from "cors";
+// import "./loadEnvironment.mjs";
+// import router from "./routes/record.mjs";
 
-const PORT = process.env.PORT || 5050;
+// const PORT = process.env.PORT || 3000;
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/record", router);
+
+// // start the Express server
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port: ${PORT}`);
+// });
+
+const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.use(cors());
-app.use(express.json());
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-app.use("/record", router);
+//Routes go here
+app.all("*", (req, res) => {
+  res.json({ "every thing": "is awesome" });
+});
 
-// start the Express server
-app.listen(PORT, () => {
-  console.log(`Server is running on port: ${PORT}`);
+//Connect to the database before listening
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("listening for requests");
+  });
 });
